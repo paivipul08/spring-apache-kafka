8e5e81a71ac04f50dd538c73f96af9a2823b4896
Vipul Pai <vipul.pai@sts.in>
2020-09-06 18:27:35 +0530

    1. Added kafka client dependency 2. Added  kafka producer beans (with callback options and with key)

---
+++ src/main/java/com/example/kafka/SpringApacheKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApacheKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApacheKafkaApplication.class, args);
+	}
+
+}
